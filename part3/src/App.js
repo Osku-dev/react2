c188d38b9b1f8dc51aefbaa5adc26b1ead3a334c
oskah <oskah@uef.fi>
2022-03-21 10:32:47 +0200

    1.12

--- part3/src/App.js
+++ part3/src/App.js
@@ -1,54 +1,33 @@
-import React, { useState } from "react";
+import { useState } from 'react'
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+const Button = ({onClick,text}) => <button onClick = {onClick}> {text} </button>;
 
-const DisplayAnecdote = ({ text, votes }) => (
-  <div>
-    {text}
-    <br></br>
-    {votes}
-  </div>
-);
 
-const App = (props) => {
-  const [selected, setSelected] = useState(0);
-  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0));
-  const newAnecdote = () => {
-    setSelected(Math.floor(Math.random() * 6));
-  };
-  const max = Math.max.apply(Math, points);
 
-  const addVote = () => {
-    const pointCopy = [...points];
-    pointCopy[selected] += 1;
-    setPoints(pointCopy);
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+
+  const handleSelected = () => {
+    setSelected(Math.floor( Math.random()*6 ) );
   };
 
   return (
     <div>
-      <h1>Anecdote of the day</h1>
-      <DisplayAnecdote
-        text={props.anecdotes[selected]}
-        votes={points[selected]}
-      />
-      <Button onClick={newAnecdote} text="generate new anecdote" />
-      <Button onClick={addVote} text="addVote" />
-      <h1>Most voted</h1>
-      <DisplayAnecdote
-        text={props.anecdotes[points.indexOf(max)]}
-        votes={max}
-      />
+      <Button onClick = {handleSelected} text = 'random anecdote' />
+      {anecdotes[selected]}
     </div>
-  );
-};
+  )
+}
 
-const anecdotes = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
-
-export default App;
+export default App
