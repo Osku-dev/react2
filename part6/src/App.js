ae9e8afe0b3d04571219d4606712a1e8ff70ad63
oskah <oskah@uef.fi>
2022-04-04 10:43:49 +0300

    2.10

--- part6/src/App.js
+++ part6/src/App.js
@@ -1,5 +1,45 @@
 import { useState } from "react";
 
+const AddForm = ({
+  newName,
+  newNumber,
+  handleNewName,
+  handleNewNumber,
+  addName,
+}) => (
+  <form onSubmit={addName}>
+    <div>
+      name: <input value={newName} onChange={handleNewName} />{" "}
+    </div>
+    <div>
+      number: <input value={newNumber} onChange={handleNewNumber} />
+    </div>
+    <div></div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+);
+
+const Filter = ({ handleNewSearch }) => (
+  <div>
+    filter: <input onChange={handleNewSearch} />{" "}
+  </div>
+);
+
+const Persons = ({ filteredList }) => (
+  <div>
+    <ul>
+      {filteredList.map((person) => (
+        <li key={person.id}>
+          {" "}
+          {person.name} {person.number}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", id: 1, number: "050488302381" },
@@ -46,30 +86,16 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter: <input onChange={handleNewSearch} />{" "}
-      </div>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNewName} />{" "}
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNewNumber} />
-        </div>
-        <div></div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter handleNewSearch={handleNewSearch} />
+      <AddForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNewName={handleNewName}
+        handleNewNumber={handleNewNumber}
+        addName={addName}
+      />
       <h2>Numbers</h2>
-      <ul>
-        {filteredList.map((person) => (
-          <li key={person.id}>
-            {" "}
-            {person.name} {person.number}
-          </li>
-        ))}
-      </ul>
+      <Persons filteredList={filteredList} />
     </div>
   );
 };
