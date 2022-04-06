2aee5fb9d119b16f122369bd01b28b2ff52e07c4
oskah <oskah@uef.fi>
2022-04-06 20:31:11 +0300

    2.12

--- part7/src/App.js
+++ part7/src/App.js
@@ -8,48 +8,55 @@ const Filter = ({ handleNewSearch }) => (
 );
 
 const Render1 = ({ country }) => {
-  console.log(country.flags)
   return (
     <div>
       <h1>{country.name.common}</h1>
-      <div><b>Capital: </b>{country.capital}</div>
-      <div><b>Population: </b>{country.population}</div>
+      <div>
+        <b>Capital: </b>
+        {country.capital}
+      </div>
+      <div>
+        <b>Population: </b>
+        {country.population}
+      </div>
 
       <h3>languages</h3>
-      <ul> 
-        {Object.keys(country.languages).map(langKey => <li key={langKey}>{country.languages[langKey]}</li>)}
+      <ul>
+        {Object.keys(country.languages).map((langKey) => (
+          <li key={langKey}>{country.languages[langKey]}</li>
+        ))}
       </ul>
 
-      <img src={country.flags} alt={`Flag of ${country.name.common}`} style={{ width: 250, height: 140 }} />
-
-     
+      <img
+        src={country.flags.png}
+        alt={`Flag of ${country.name.common}`}
+        style={{ width: 250, height: 140 }}
+      />
     </div>
-  )
-}
+  );
+};
 
 const Render10 = ({ filteredList }) =>
-  filteredList.map((country) => <div key={country.name.common}>{country.name.common}</div>);
+  filteredList.map((country) => (
+    <div key={country.name.common}>{country.name.common}</div>
+  ));
 
-  const RenderMaster = ({ filteredList}) => {
-    if (filteredList.length === 1) {
-      return (<Render1 country={filteredList[0]} />)
-    }
-    else if (filteredList.length < 10) {
-      return (<Render10 filteredList={filteredList} />)
-    }
-    return (<div>Too many matches</div>)
+const RenderMaster = ({ filteredList }) => {
+  if (filteredList.length === 1) {
+    return <Render1 country={filteredList[0]} />;
+  } else if (filteredList.length < 10) {
+    return <Render10 filteredList={filteredList} />;
   }
-  
-  
+  return <div>Too many matches</div>;
+};
 
 const App = () => {
   const [countries, setCountries] = useState([]);
 
   const [newSearch, setNewSearch] = useState("");
-  useEffect(() => { 
+  useEffect(() => {
     axios.get("https://restcountries.com/v3.1/all").then((response) => {
       setCountries(response.data);
-      
     });
   }, []);
 
