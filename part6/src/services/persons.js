5dee86232d2a6c7c56a1c5f2b4d44ed7d1593959
oskah <oskah@uef.fi>
2022-04-12 15:17:47 +0300

    2.18 kesken

--- part6/src/services/persons.js
+++ part6/src/services/persons.js
@@ -11,6 +11,12 @@ const create = (newObject) => {
   return request.then((response) => response.data);
 };
 
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+
 const deletePerson = (id) => {
   const request = axios.delete(`${baseUrl}/${id}`);
   return request.then((response) => response.status);
@@ -19,6 +25,7 @@ const deletePerson = (id) => {
 const exportedObject = {
   getAll,
   create,
+  update,
   deletePerson,
 };
 
