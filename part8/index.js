ce0cee955f433470654f16aa5dd45ed14fa55f65
oskah <oskah@uef.fi>
2022-04-27 16:17:08 +0300

    3.7

--- part8/index.js
+++ part8/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+app.use(morgan('tiny'))
+
+
 
 let persons =  [
     {
@@ -80,6 +85,8 @@ app.get('/api/persons/:id', (request, response) => {
     persons = persons.concat(person)
   
     response.json(person)
+    
+    morgan.token(person)
   })
 
 const PORT = 3001
