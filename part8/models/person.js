1eb45d494535bad1887ad381162bf70bbf4d2de5
oskah <oskah@uef.fi>
2022-06-08 12:55:04 +0300

    3.20

--- part8/models/person.js
+++ part8/models/person.js
@@ -19,7 +19,6 @@ const personSchema = new mongoose.Schema({
   },
   number: { 
     type: String,
-    minlength: 8,
     validate: {
       validator: function(v) {
         return /\d{2}-\d6*|\d{3}-\d5*/.test(v);
