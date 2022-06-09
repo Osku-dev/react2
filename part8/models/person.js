bb7ab2315842ccd451849399d9f660ab9bbbf8ca
oskah <oskah@uef.fi>
2022-06-09 13:57:33 +0300

    3.22

--- part8/models/person.js
+++ part8/models/person.js
@@ -1,38 +1,39 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
+const process = require('process')
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
+  .then(() => {
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: 3 
+    minlength: 3
   },
-  number: { 
+  number: {
     type: String,
     validate: {
       validator: function(v) {
-        return /\d{2}-\d6*|\d{3}-\d5*/.test(v);
-      }}
+        return /\d{2}-\d6*|\d{3}-\d5*/.test(v)
+      } }
   }
-});
+})
 
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
--- part8/mongo.js
+++ part8/mongo.js
@@ -1,38 +1,38 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
+const process = require('process')
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.wag1t.mongodb.net/personApp?retryWrites=true&w=majority`;
-
-mongoose.connect(url);
+const url = `mongodb+srv://fullstack:${password}@cluster0.wag1t.mongodb.net/personApp?retryWrites=true&w=majority`
+mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: Number,
-});
+})
 
-const Person = mongoose.model("Person", personSchema);
+const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
   name: process.argv[3],
   number: process.argv[4],
-});
+})
 
 if (process.argv[3] && process.argv[4] !== null) {
-  person.save().then((result) => {
-    console.log(`added ${person.name} number ${person.number} to phonebook`);
-    mongoose.connection.close();
-  });
+  person.save().then(() => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
 } else {
   Person.find({}).then((result) => {
     result.forEach((person) => {
-      console.log(person);
-    });
-    mongoose.connection.close();
-  });
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })
 }
